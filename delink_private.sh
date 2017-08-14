782f30016baafed3656794800a75f049310fd83a
Ali <amaal.ali@sage017669.sageukie.adinternal.com>
2017-08-14 10:28:38 +0100

    Remove link file till I fix stow ignore list.

--- delink_private.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# This script delinks the dotfiles from the system
-
-rm ~/.bash_profile
-rm ~/.gitconfig
-rm ~/.tmux.conf
-rm ~/.vimrc
--- link_private.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# This script will sets up the dotfiles
-
-ln -nfs ./.bash_profile ~/.bash_profile
-ln -nfs ./.gitconfig ~/.gitconfig
-ln -nfs ./.tmux.conf ~/.tmux.conf
-ln -nfs ./.vimrc ~/.vimrc
-# ln -nfs ./vim/ ~/.vim
