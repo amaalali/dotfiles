2f2f1e5444ff03a1e041a1dac1e8ef01498355e9
Amaal Ali <9220299+amaalali@users.noreply.github.com>
2023-03-29 23:15:38 +0100

    Superficial improvement to logging

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo "[START] Running setup for dotfiles."
+echo "Level=[INFO] Stage=[START] Details=[Running setup for dotfiles]"
 
 TPM_TARGET_DIR=~/.tmux/plugins/tpm
 if [ -d "$TPM_TARGET_DIR" ]; then
-  echo "[INFO][TPM] Skipping TPM installation as target directory already exists at '$TPM_TARGET_DIR'."
+  echo "Level=[INFO] Stage=[TPM] Details=[Skipping TPM installation as target directory already exists at '$TPM_TARGET_DIR']"
 else
-  echo "[INFO][TPM] Intalling TPM to target directory '$TPM_TARGET_DIR'"
+  echo "Level=[INFO] Stage=[TPM] Details=[Installing TPM to target directory '$TPM_TARGET_DIR']"
   git clone https://github.com/tmux-plugins/tpm $TPM_TARGET_DIR
 fi
 
-echo "[END] Setup complete."
+echo "Level=[INFO] Stage=[END] Details=[Setup complete]"
 
