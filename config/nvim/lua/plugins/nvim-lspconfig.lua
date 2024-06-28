ed5e9e860aeb4621b366fa0b9357a1ced54d5434
Amaal Ali <9220299+amaalali@users.noreply.github.com>
2024-06-28 10:57:53 +0100

    feat(lsp): enable pyright for python lsp

--- config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -110,14 +110,10 @@ return { -- LSP Configuration & Plugins
     local servers = {
       -- clangd = {},
       -- gopls = {},
-      -- pyright = {},
+      pyright = {},
       -- rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-      --
-      -- Some languages (like typescript) have entire language plugins that can be useful:
-      --    https://github.com/pmizio/typescript-tools.nvim
-      --
-      -- But for many setups, the LSP (`tsserver`) will work just fine
+
       tsserver = {},
 
       lua_ls = {
