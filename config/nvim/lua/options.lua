758b89560179093eeef24fb23a9cc1c5a962a78a
Amaal Ali <9220299+amaalali@users.noreply.github.com>
2024-08-29 16:13:11 +0100

    feat(tabs): experiment with setting tabs in nvim at size of 4

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -54,7 +54,7 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
-vim.cmd 'set expandtab'
-vim.cmd 'set tabstop=2'
-vim.cmd 'set softtabstop=2'
-vim.cmd 'set shiftwidth=2'
+vim.cmd 'set autoindent'
+vim.cmd 'set noexpandtab'
+vim.cmd 'set tabstop=4'
+vim.cmd 'set shiftwidth=4'
